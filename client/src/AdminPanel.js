6dcf521428c5f3c8cccf98012261f52196a294a4
sachin0034 <161276831+sachin0034@users.noreply.github.com>
2024-07-30 15:14:31 +0530

    Update AdminPanel.js

--- client/src/AdminPanel.js
+++ client/src/AdminPanel.js
@@ -19,8 +19,8 @@ function AdminPanel() {
 
   const connectWebSocket = () => {
     const protocol = window.location.protocol === 'https:' ? 'wss' : 'ws';
-    // const wsUrl = `${protocol}://watch-dog-llm-nn5u.vercel.app`;
-    const wsUrl = "https://watch-dog-llm.vercel.app";
+    const wsUrl = `${protocol}://watch-dog-llm.vercel.app`;
+    // const wsUrl = "https://watch-dog-llm.vercel.app";
     // const wsUrl = "http://localhost:5000";
     ws.current = new WebSocket(wsUrl);
 
