730da1e1ce9373a4b3f461d972641afae0b7354f
sachin0034 <161276831+sachin0034@users.noreply.github.com>
2024-07-30 14:43:20 +0530

    Backend_URL

--- client/src/UserChat.js
+++ client/src/UserChat.js
@@ -18,7 +18,8 @@ function UserChat() {
 
   const connectWebSocket = () => {
     const protocol = window.location.protocol === 'https:' ? 'wss' : 'ws';
-    const wsUrl = `${protocol}://watch-dog-llm-nn5u.vercel.app`;
+    // const wsUrl = `${protocol}://watch-dog-llm-nn5u.vercel.app`;
+    const wsUrl = `https://watch-dog-llm.vercel.app/user`;
     // const wsUrl = "http://localhost:5000";
     ws.current = new WebSocket(wsUrl);
 
